bf82763fab2f4e5b1542375fdfde3d150c69e0dd
Connor Zhao <connor.zhao@infotrack.com.au>
2017-04-12 15:32:04 +1000

    use browsertime to test website speed of home,Nsw,Vic

--- test/prepostscripts/preLoginExample.js
+++ test/prepostscripts/preLoginExample.js
@@ -8,7 +8,7 @@ module.exports = {
   run(context) {
     return context.runWithDriver((driver) => {
       // Go to Wikipedias login URL
-      return driver.get('https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page')
+      return driver.get('https://search.infotrack.com.au')
         .then(() => {
           // You need to find the form, the login input fields and the
           // password field. Just add you name and password and submit the form
@@ -18,12 +18,12 @@ module.exports = {
           // we fetch the selenium webdriver from context
           var webdriver = context.webdriver;
           // before you start, make your username and password
-          var userName = 'YOUR_USERNAME_HERE';
-          var password = 'YOUR_PASSWORD_HERE';
+          var userName = 'Infotrack9';
+          var password = '123Infotrack';
           var loginForm = driver.findElement(webdriver.By.css('form'));
-          var loginInput = driver.findElement(webdriver.By.id('wpName1'));
+          var loginInput = driver.findElement(webdriver.By.id('username'));
           loginInput.sendKeys(userName);
-          var passwordInput = driver.findElement(webdriver.By.id('wpPassword1'));
+          var passwordInput = driver.findElement(webdriver.By.id('password'));
           passwordInput.sendKeys(password);
           return loginForm.submit();
           // this example skips waiting for the next page and validating that the login was successful.
